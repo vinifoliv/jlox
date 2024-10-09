a420ea74abfb5e2afe541082d2661d6e7d690c5e
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-08 23:22:07 -0300

    I added a Makefile for automatically compiling and running the AST generation code. Also, because of it, the Expr class (result of the AST generator) was added.

---
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,66 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+// Automatically generates an AST (Expr.java)
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal : Object value",
+            "Unary : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(
+        String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        // Subclasses definition
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("\tstatic class " + className + " extends " + baseName + " {");
+
+        // Constructor
+        writer.println("\t\t" + className + "(" + fieldList + ") {");
+
+        // Parameters to fields
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("\t\t\tthis." + name + " = " + name + ";");
+        }
+
+        writer.println("\t\t}\n");
+        for (String field : fields) {
+            writer.println("\t\tfinal " + field + ";");
+        }
+
+        writer.println("\t}\n");
+    }
+}
